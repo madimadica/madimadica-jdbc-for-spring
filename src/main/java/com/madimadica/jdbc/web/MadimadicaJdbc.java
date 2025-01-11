4d686e3778c512ed5e942d9f0a2e9e7b6c14b16e
Madi <145222821+madimadica@users.noreply.github.com>
2025-01-11 15:32:49 -0600

    Add config root

---
+++ src/main/java/com/madimadica/jdbc/web/MadimadicaJdbc.java
@@ -0,0 +1,9 @@
+package com.madimadica.jdbc.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.madimadica.jdbc.web")
+public class MadimadicaJdbc {
+}
