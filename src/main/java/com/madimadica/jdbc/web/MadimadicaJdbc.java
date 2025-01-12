bd4686a39225e9fb5bb7e21f1dddad20bff6ff6e
Madi <145222821+madimadica@users.noreply.github.com>
2025-01-11 23:35:46 -0600

    MadimadicaJdbc: Add queryString

--- src/main/java/com/madimadica/jdbc/web/MadimadicaJdbc.java
+++ src/main/java/com/madimadica/jdbc/web/MadimadicaJdbc.java
@@ -332,6 +332,41 @@ public interface MadimadicaJdbc {
     }
 
     /**
+     * Run a query with no parameters for an {@link Optional} {@link String}.
+     * The result is empty if no rows were returned, and present if 1 row was returned.
+     * @param sql SQL query
+     * @return an optionally mapped row.
+     * @throws IncorrectResultSizeDataAccessException if the query results more than 1 row
+     */
+    default Optional<String> queryString(String sql) {
+        return queryOneHelper(queryStrings(sql));
+    }
+
+    /**
+     * Run a query with varargs parameters for an {@link Optional} {@link String}.
+     * The result is empty if no rows were returned, and present if 1 row was returned.
+     * @param sql SQL query
+     * @param args varargs parameters, which are flattened according to {@link FlattenedParameters#of(String, Object...)}
+     * @return an optionally mapped row.
+     * @throws IncorrectResultSizeDataAccessException if the query results more than 1 row
+     */
+    default Optional<String> queryString(String sql, Object... args) {
+        return queryOneHelper(queryStrings(sql, args));
+    }
+
+    /**
+     * Run a query with named parameters for an {@link Optional} {@link String}.
+     * The result is empty if no rows were returned, and present if 1 row was returned.
+     * @param sql SQL query
+     * @param namedArgs map of named parameters
+     * @return an optionally mapped row.
+     * @throws IncorrectResultSizeDataAccessException if the query results more than 1 row
+     */
+    default Optional<String> queryString(String sql, Map<String, ?> namedArgs) {
+        return queryOneHelper(queryStrings(sql, namedArgs));
+    }
+
+    /**
      * <p>
      *     Perform a row update query based on the given parameter object.
      * </p>
